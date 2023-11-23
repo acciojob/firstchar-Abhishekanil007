b3bd66c8a712fe04f3e54b2bec2f08679a88f7bc
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-23 21:02:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function firstChar(text) {
   // your code here
+	let result = '';
+	if(text.length === 0)
+	{
+		return result;
+	}
+	else{
+
+		for(let i=0; i<text.length; i++)
+			{
+				let charCode = character.charCodeAt(i);
+				if(text.charAt(i) === charCode)
+				{
+					result+=text.charAt(i);
+					return result;
+				}
+					
+			}
+		
+	}
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
